07b29cabe5211c5a1f25c8a4b0444b367d58947c
徐正昀 <xuzy@go-goal.com>
2021-08-25 09:33:00 +0800

    增加空布局模板

---
+++ src/layout/empty/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="empty-div">
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: 'empty-layout',
+}
+</script>
+<style>
+  .empty-div {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+  }
+</style>

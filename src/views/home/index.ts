7a3452a299be6f215447e124adf99f1b13e6be86
徐正昀 <xuzy@go-goal.com>
2022-02-07 11:34:53 +0800

    修点bug

--- src/views/home/index.ts
+++ src/views/home/index.ts
@@ -1,9 +1,10 @@
-import { useRoute, Router } from 'vue-router';
+import { useRoute, RouteLocationNormalizedLoaded, useRouter, Router } from 'vue-router';
 
 export const components = {};
 
 export const setup = function (props? :any, context? :any) {
-  const route: Router = useRoute();
+  const route: RouteLocationNormalizedLoaded = useRoute();
+  const router: Router = useRouter();
   console.log('in');
   return {
   };
